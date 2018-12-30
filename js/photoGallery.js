f6aa657f938fc1b8e1d26fead3792f5ab25945c3
Alexander Muylaert <alexander.muylaert@ugent.be>
2018-12-30 21:37:49 +0100

    fixed typo

--- js/photoGallery.js
+++ js/photoGallery.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var dir = "gallery/images/2010/2010_1(";
     var i = 1;
     while (i < 61) {
-      $("#images").append('<a class="lightbox" href="'+dir+i+'").JPG"> <img src="'+dir+i+').JPG" alt="2010"> </a>');
+      $("#images").append('<a class="lightbox" href="'+dir+i+').JPG"> <img src="'+dir+i+').JPG" alt="2010"> </a>');
       i++;
     }
 
