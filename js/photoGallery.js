507f29112e34fbd61cc19b457a926bcf8bd6bb05
Alexander Muylaert <alexander.muylaert@ugent.be>
2018-12-31 01:35:06 +0100

    added thumbnails for faster loading

--- js/photoGallery.js
+++ js/photoGallery.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
 
     var dir = "gallery/images/2010/2010_1(";
+    var thumbdir = "gallery/thumbnail/2010/2010_1(";
     var i = 1;
     while (i < 61) {
-      $("#images").append('<a class="lightbox" href="'+dir+i+').JPG"> <img src="'+dir+i+').JPG" alt="2010"> </a>');
+      $("#images").append('<a class="lightbox" href="'+dir+i+').JPG"> <img src="'+thumbdir+i+')-min.JPG" alt="2010"> </a>');
       i++;
     }
 
