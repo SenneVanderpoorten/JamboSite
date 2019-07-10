ee19d4e34e9079f40f3055343360cc981b8c7cbd
pieterism <pervdh@gmail.com>
2019-07-10 15:19:15 +0200

    update newstable TODO: live map

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -1,5 +1,18 @@
-
-
-$.getJSON("news/news.json", function(json) {
-    console.log(json); // show the JSON file content into console
+$(document).ready(function showNews() {
+    $.getJSON("news/news.json", function (data) {
+        let res = "<div class='news-table'>";
+        $.each(data, function (key, value) {
+            res += "<a href='" + value["link"] + "' target='_blank'>";
+            res += "<li class='table-row'>";
+            res += "<div class='col-8'>" + value["titel"] + "</div>";
+            res += "<div class='col-2'>" + value["datum"] + "</div>";
+            res += "<div class='col-2'>" + value["krant"] + "</div>";
+            res += "</li>";
+            res += "</a>";
+        });
+        res += "</div>";
+        document.getElementById("newsTable").innerHTML = res;
+        console.log(res);
+    });
 });
+
