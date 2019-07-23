8324cdf508a31129d51e34b8742b3176d29e7386
pieterism <pervdh@gmail.com>
2019-07-23 11:32:13 +0200

    update nl language, TODO: fix references

--- js/photoGallery.js
+++ js/photoGallery.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
     let thumbdir;
 
     if(window.location.pathname.split("/")[1] == "languages"){
-      dir = "https://jamborette.be//gallery/images/2010/2010_1(";
-      thumbdir = "https://jamborette.be//gallery/thumbnail/2010/2010_1(";
+      dir = "/gallery/images/2010/2010_1(";
+      thumbdir = "/gallery/thumbnail/2010/2010_1(";
     }
     else{
-      dir = "https://jamborette.be//gallery/images/2010/2010_1(";
-      thumbdir = "https://jamborette.be//gallery/thumbnail/2010/2010_1(";
+      dir = "/gallery/images/2010/2010_1(";
+      thumbdir = "/gallery/thumbnail/2010/2010_1(";
     }
     var i = 1;
     while (i < 61) {
