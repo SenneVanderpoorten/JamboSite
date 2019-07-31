fa3c84ea1eaeb5900224deaca8ccd8a9b6d24d0f
pieterism <pervdh@gmail.com>
2019-07-31 11:49:40 +0200

    update multi languages

--- js/languages.js
+++ js/languages.js
@@ -1,12 +1,32 @@
 const languages = {
-    "de": {},
-    "es": {},
-    "fr": {},
-    "it": {},
-    "nl": {},
-    "no": {},
+    en:{
+        'history': 'history',
+        'information': 'information',
+        'activities': 'activities ',
+        'organisation': 'organisation',
+        'interested': 'interested?',
+        'pictures': 'pictures',
+        'newsfeed': 'newsfeed',
+        'contact': 'contact '},
+    de: {
+        'history': 'GESCHICHTE',
+        'information': 'INFORMATION',
+        'activities': 'AKTIVITÄTEN ',
+        'organisation': 'ORGANISATION',
+        'interested': 'INTERESSIERT?',
+        'pictures': 'FOTO’S',
+        'newsfeed': 'Nachrichtenüberblick',
+        'contact': 'KONTAKT '
+    },
+    es: {},
+    fr: {},
+    it: {},
+    nl: {},
+    no: {},
 }
 
 function loadLanguage(lang) {
-    console.log(lang);
+    $('.lang').each(function (index, element) {
+        $(this).text(languages[lang][$(this).attr('key')]);
+    })
 }
