22d586547da87977a00c31511925f670d16a410f
pieterism <pervdh@gmail.com>
2020-03-16 22:17:26 +0100

    update counter buttons

--- js/payment.js
+++ js/payment.js
@@ -1,24 +1,36 @@
 
-//$(".toast").toast('show');
-
 function closeToast() {
         document.getElementById("paymentToast").style.display = 'none';
         console.log('close toast')
 }
 
+function increment_input(){
+    console.log("plus button")
+}
+
+function decrement_input() {
+    console.log("minus button")
+}
+
+function increment_veggie() {
+
+}
+
 $(document).ready(function() {
-        $('.minus').click(function () {
-                var $input = $(this).parent().find('input');
-                var count = parseInt($input.val()) - 1;
-                count = count < 1 ? 1 : count;
-                $input.val(count);
-                $input.change();
-                return false;
-        });
-        $('.plus').click(function () {
-                var $input = $(this).parent().find('input');
-                $input.val(parseInt($input.val()) + 1);
-                $input.change();
-                return false;
-        });
+    $('.minus-btn').click(function () {
+
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus-btn').click(function () {
+
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
 });
