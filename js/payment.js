d5833ce8b320404a6a8aa24ddfb0511258b6a087
pieterism <pervdh@gmail.com>
2020-03-16 21:56:10 +0100

    update payment html

--- js/payment.js
+++ js/payment.js
@@ -5,3 +5,20 @@ function closeToast() {
         document.getElementById("paymentToast").style.display = 'none';
         console.log('close toast')
 }
+
+$(document).ready(function() {
+        $('.minus').click(function () {
+                var $input = $(this).parent().find('input');
+                var count = parseInt($input.val()) - 1;
+                count = count < 1 ? 1 : count;
+                $input.val(count);
+                $input.change();
+                return false;
+        });
+        $('.plus').click(function () {
+                var $input = $(this).parent().find('input');
+                $input.val(parseInt($input.val()) + 1);
+                $input.change();
+                return false;
+        });
+});
