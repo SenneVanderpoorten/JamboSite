d7de864a8b79a4db962edf6b49ee43b2b0af675c
pieterism <pervdh@gmail.com>
2020-03-18 09:47:57 +0100

    updat cart total counter

--- js/payment.js
+++ js/payment.js
@@ -4,33 +4,36 @@ function closeToast() {
         console.log('close toast')
 }
 
-function increment_input(){
-    console.log("plus button")
+function increment_input(id){
+    const value = parseInt(document.getElementById(id).value) + 1;
+    document.getElementById(id).value = value;
+    update_total()
 }
 
-function decrement_input() {
-    console.log("minus button")
+function decrement_input(id) {
+    let value =0;
+    if (parseInt(document.getElementById(id).value) === 0){
+        value = 0;
+    } else{
+        value = parseInt(document.getElementById(id).value) - 1;
+
+    }
+
+    document.getElementById(id).value = value;
+    update_total()
 }
 
-function increment_veggie() {
-
+function update_total() {
+    const bbq_adult_amount = parseInt(document.getElementById('cart-count-18').value);
+    const bbq_kid_amount = parseInt(document.getElementById('cart-count-14').value);
+    const veggie_adult_amount = parseInt(document.getElementById('cart-count-veggie-18').value);
+    const veggie_kid_amount = parseInt(document.getElementById('cart-count-veggie-14').value);
+    const total_price = 
+    console.log('CART');
+    console.log('-----');
+    console.log('BBQ Adult: ', bbq_adult_amount);
+    console.log('BBQ Kid: ', bbq_kid_amount);
+    console.log('Veggie Adult: ', veggie_adult_amount);
+    console.log('Veggie Kid: ', veggie_kid_amount);
+    console.log('TOTAL: ', total_price);
 }
-
-$(document).ready(function() {
-    $('.minus-btn').click(function () {
-
-        var $input = $(this).parent().find('input');
-        var count = parseInt($input.val()) - 1;
-        count = count < 1 ? 1 : count;
-        $input.val(count);
-        $input.change();
-        return false;
-    });
-    $('.plus-btn').click(function () {
-
-        var $input = $(this).parent().find('input');
-        $input.val(parseInt($input.val()) + 1);
-        $input.change();
-        return false;
-    });
-});
